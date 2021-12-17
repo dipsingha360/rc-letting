9d1e19476f2c90b52f4a7e943781b9ecbfdc35da
dipsingha360 <dipsingha2@gmail.com>
2021-12-17 11:00:54 +0600

    Hamburger menu first functionality added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log("Checking");
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click',function() {
+    console.log("HamBurger clicked!");
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open')
+
+    } else{
+        btnHamburger.classList.add('open')
+    }
+    
+})
